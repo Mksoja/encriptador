b25973a9a2da0d517449b9c7582390c117bfd41b
kazucruz <k4sofia3006@gmail.com>
2023-08-15 22:09:52 -0500

    creacion carpeta js

---
+++ script/script.js
@@ -0,0 +1,18 @@
+//function mostrarImagen(){
+    const mensaje_encriptado= document.querySelector("#msj_encriptado");
+    console.log(mensaje_encriptado);
+    //const imagen_muñeco = document.querySelector(".img_munieco");
+    //if(mensaje_encriptado == " ") {
+        
+   // }
+//}  
+function encriptar(){
+    const texto = documen.getElementById("texto_ingresado").value.toLowerCase();
+    var texto_ingresado = texto.replace(/e/img,"enter");
+    var texto_ingresadoo = texto.replace(/o/img,"ober");
+    var texto_ingresadoo = texto.replace(/i/img,"imes");
+    var texto_ingresadoo = texto.replace(/a/img,"ai");
+    var texto_ingresadoo = texto.replace(/u/img,"ufat");
+
+    
+}
